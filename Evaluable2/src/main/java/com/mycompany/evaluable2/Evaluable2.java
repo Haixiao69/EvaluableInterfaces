9cb89e6a35e9356e595c47388956ea5f87c2ea3d
Haixiao <hwa0001@alu.medac.es>
2025-11-05 09:03:59 +0100

    commit

---
+++ Evaluable2/src/main/java/com/mycompany/evaluable2/Evaluable2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.evaluable2;
+
+/**
+ *
+ * @author PC229
+ */
+public class Evaluable2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
